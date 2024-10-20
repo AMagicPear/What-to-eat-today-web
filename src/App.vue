f80779a4b03ee20d9d610b1ddd680521a7203c22
AMagicPear <AMagicPear@outlook.com>
2024-10-20 14:59:37 +0800

    学习Vue截止分支

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
   <ion-app>
     <ion-router-outlet />
   </ion-app>
+  <person-learn/>
 </template>
 
 <script setup lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { IonApp, IonRouterOutlet } from '@ionic/vue'; //用于ionic应用的路由
+// import PersonLearn from './components/PersonLearn.vue'
 </script>
--- src/components/PersonLearn.vue
+++ src/components/PersonLearn.vue
@@ -1,20 +1,20 @@
 <template>
-    <div class="person">
-        <h2>当前求和为：{{ sum }}</h2>
-        <button @click="add">点我sum+1</button>
-    </div>
+    <ion-page>
+        <ion-content>
+            <IonLabel>当前求和为：{{ sum }}</IonLabel>
+            <ion-button @click="add">点我sum+1</ion-button>
+        </ion-content>
+    </ion-page>
 </template>
 
 <script lang="ts" setup name="PersonLearn">
 import { ref } from 'vue'
+import { IonPage, IonButton, IonContent, IonLabel } from '@ionic/vue';
 //数据
 const sum = ref(0)
-//方法
-function add() {
-    sum.value += 1;
+const add = () => {
+    sum.value+=1
 }
-//创建
-console.log('创建')
-//挂载
-console
 </script>
+
+<style scoped></style>
