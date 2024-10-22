9c7aa6d0923ad9155a1fe952494fcc42e657def2
AMagicPear <AMagicPear@outlook.com>
2024-10-22 11:44:03 +0800

    Implement food storage logic and add food addition functionality

--- src/composables/foodCalculator.ts
+++ src/composables/foodCalculator.ts
@@ -1,4 +1,6 @@
 import { ref } from 'vue';
+import storage from './storage';
+import { errorMessage } from '@/views/EditFood.vue';
 class Food {
     name: string;
     weight: number;
@@ -9,13 +11,58 @@ class Food {
         this.weight = weight;
         this.counts = counts;
     }
+    toStorage() {
+        return {
+            name: this.name,
+            weight: this.weight,
+            counts: this.counts
+        }
+    }
+    static fromStorage(json: any) {
+        return new Food(json.name, json.weight, json.counts);
+    }
 }
 const foodList = ref<Food[]>([]);
 
 export const FoodCalculator = () => {
-    
+    const loadFoods = async () => {
+        console.log("正在加载食物，加载前：");
+        console.log(foodList.value);
+        const storedFoods = await storage.get('foodList');
+        console.log("读取到的存储：");
+        console.log(storedFoods);
+        if (storedFoods)
+            foodList.value = storedFoods.map((food: any) => Food.fromStorage(food));
+        console.log("加载后：");
+        console.log(foodList.value);
+    }
+    const saveFoods = async () => {
+        const plainFoods = foodList.value.map(food => food.toStorage());
+        console.log(plainFoods);
+        await storage.set('foodList', plainFoods);
+    }
+    const addFood = async (name: string, weight: number) => {
+        if (foodList.value.find(food => food.name === name)) {
+            errorMessage.value = "已经存在该食物，请勿重复添加";
+            return false;
+        }
+        console.log(`正在添加食物${name}，${weight}`);
+        foodList.value.push(new Food(name, weight));
+        await saveFoods();
+        return true;
+    }
+    const deleteFood = async (name: string) => {
+        console.log(`正在删除食物${name}`);
+        foodList.value = foodList.value.filter(food => food.name !== name);
+        await saveFoods();
+        return true;
+    }
+
     return {
-        Food,
+        loadFoods,
         foodList,
+        addFood,
+        saveFoods,
+        deleteFood,
     }
 }
--- src/views/EditFood.vue
+++ src/views/EditFood.vue
@@ -1,16 +1,14 @@
 <template>
   <ion-page>
     <ion-content>
-      <ion-note class="make-central">编辑食物表格的页面</ion-note>
+      <ion-note class="make-central">编辑食物表格的页面<br />{{ foodList }}</ion-note>
       <ion-fab slot="fixed" vertical="bottom" horizontal="end" id="add-button">
         <ion-fab-button>
           <ion-icon :icon="add"></ion-icon>
         </ion-fab-button>
       </ion-fab>
     </ion-content>
-    <ion-alert header="添加食物" trigger="add-button" :buttons=alertButtons :inputs=alertInputs>
-      <!-- <div v-if="errorMessage" class="error-message">{{ errorMessage }}</div> -->
-    </ion-alert>
+    <ion-alert header="添加食物" trigger="add-button" :buttons=alertButtons :inputs=alertInputs></ion-alert>
     <ion-toast :is-open="isAdmitSuccessOpen" message="添加成功！" :duration="2000"
       @didDismiss="isAdmitSuccessOpen = false"></ion-toast>
     <ion-toast :is-open="errorMessage" :message="errorMessage" :duration="2000"
@@ -21,37 +19,40 @@
 <script lang="ts">
 import { IonPage, IonContent, IonFab, IonFabButton, IonIcon, IonNote, IonAlert, IonToast } from '@ionic/vue';
 import { add } from 'ionicons/icons';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { FoodCalculator } from '@/composables/foodCalculator';
-const { Food, foodList } = FoodCalculator();
+const { foodList, addFood, loadFoods } = FoodCalculator();
+export const errorMessage = ref<string | null>(null);
 </script>
 
 <script setup lang="ts">
-
 //页面逻辑
 const isAdmitSuccessOpen = ref(false);
-const errorMessage = ref<string | null>(null);
 const alertButtons = [
   {
     text: '取消',
     role: 'cancel',
     handler: () => {
       errorMessage.value = null;
-      console.log('取消');
+      console.log('已取消添加食物');
     },
   },
   {
     text: '添加',
-    handler: (data: any) => {
+    handler: async (data: any) => {
       if (!(data.foodName && data.foodWeight)) {
         console.log('食物未输入')
-        errorMessage.value = '请输入正确的食物名称和重量';
-        return false
+        errorMessage.value = '请输入正确的食物名称和重量'; //显示条形通知，提示格式不正确
+        return false;
       }
       else {
-        foodList.value?.push(new Food(data.foodName, data.foodWeight));
-        console.log(foodList.value);
-        isAdmitSuccessOpen.value = true; //显示条形通知，表示添加成功
+        console.log(`准备添加食物${data.foodName}，${data.foodWeight}`);
+        if (await addFood(data.foodName, data.foodWeight)) {
+          isAdmitSuccessOpen.value = true; //显示条形通知，表示添加成功
+          return true;
+        } else {
+          return false;
+        }
       }
     }
   }
@@ -68,9 +69,9 @@ const alertInputs = [
     min: 0,
   },
 ];
-
-//读取食物信息
-
+onMounted(() => {
+  loadFoods();
+})
 </script>
 
 <style scoped>
