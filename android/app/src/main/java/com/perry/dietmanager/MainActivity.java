12469f462292c1db124e3e35a5b1a5d657febf6f
AMagicPear <AMagicPear@outlook.com>
2024-10-23 12:53:07 +0800

    Ready for build

--- android/app/src/main/java/com/perry/dietmanager/MainActivity.java
+++ android/app/src/main/java/com/perry/dietmanager/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.perry.dietmanager;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,16 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'myApp',
+  appId: 'com.perry.dietmanager',
+  appName: '今天吃什么',
   webDir: 'dist'
-};
+,
+    android: {
+       buildOptions: {
+          keystorePath: '/Users/amagicpear/自己的一些可执行程序/dietappkey',
+          keystoreAlias: 'dietappkey',
+       }
+    }
+  };
 
 export default config;
